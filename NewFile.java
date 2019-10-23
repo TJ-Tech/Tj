835ee6d6e800ff8f0bb12ff6965969a8408e90e6
TJ-Tech <56900098+TJ-Tech@users.noreply.github.com>
2019-10-22 20:32:56 -0400

    Techno Study replace to TxCHNxSTxDY

--- NewFile.java
+++ NewFile.java
@@ -1,1 +1,10 @@
-class Program 
+public static void main(String[] args) {
+
+
+        //Write a method that would replace every vowel in the string with X.
+        // hint you have to use .char
+        String input = "Technostudy";
+        char x = input.charAt(4);
+        input = input.replaceAll("[e,o,u]", "X");
+        System.out.println(input);
+    }
